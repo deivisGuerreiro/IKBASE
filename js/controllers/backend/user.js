ed20b357bd52d7cd57293ec2c6dab5f3130c7ed5
Deivis Felipe <deivis.guerreiro@gmail.com>
2019-12-17 10:14:02 -0300

    cotroller user

--- js/controllers/backend/user.js
+++ js/controllers/backend/user.js
@@ -1,17 +1,8 @@
 const db = require("../../../conexao.js")
 
-const getAll = async () => {
+const insert = async (nome,email,senha) => {
 
-  const query = "select * from prefeitos"
-  result = await db.query(query)
-
-  console.log(result.rows);
-  return result.rows
-}
-
-const insert = async (id_user,duvida,tecnologias) => {
-
-  const query = "insert into usuario (nome,email,senha) values ($1,$2,$3)"
+  const query = "insert into usuarios (nome,email,senha) values ($1,$2,$3)"
   result = await db.query(query,[nome,email,senha])
 
   console.log(result.rows);
@@ -26,7 +17,7 @@ const update = async (nome,email,id) => {
     console.log("ITEM INEXISTENTE");
     return "ITEM INEXISTENTE"
   }
-  const query = "update usuario set nome = $1, email = $2 where id=$3"
+  const query = "update usuarios set nome = $1, email = $2 where id=$3"
   result = await db.query(query,[nome,email,id])
 
   console.log(result.rows);
@@ -35,13 +26,13 @@ const update = async (nome,email,id) => {
 
 const deletar = async (id) => {
 
-  const verifica = "select * from usuario where id = $1"
+  const verifica = "select * from usuarios where id = $1"
   var result = await db.query(verifica,[id])
   if(!result.rows.length > 0){
     console.log("ITEM INEXISTENTE");
     return "ITEM INEXISTENTE"
   }
-  const query = "delete from usuario where id=$1"
+  const query = "delete from usuarios where id=$1"
   await db.query(query,[id])
 
   console.log("ITEM DELETADO");
@@ -52,12 +43,21 @@ const deletar = async (id) => {
 const get = async (id) => {
 
   var result
-  const query = "select * from usuario where id = $1";
+  const query = "select * from usuarios where id = $1";
   result = await db.query(query,[id])
 
 
   return result.rows
   
+}
+
+const getAll = async() => {
+   var result
+   const query = " SELECT * FROM  usuarios"
+
+   result = await db.query(query)
+
+   return result.rows
 }
 
 module.exports = {
