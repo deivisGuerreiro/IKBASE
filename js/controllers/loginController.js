3eae805c495d1eab9864c2d0b88ece5e65ab095e
Diorge Nunes <diorgeandreinunes@gmail.com>
2019-12-17 08:38:21 -0300

    feat(auth): adicionar modulo de autenticação

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,6 +1,15 @@
-angular.module('ikbaseApp').controller('loginController', ($cope, $routeParams) => {
+angular.module('ikbaseApp').controller('loginController', ($scope, $routeParams, authService) => {
+    $scope.login = function (usuario) {
 
-
-
-    
+        authService.login(usuario)
+          .then(
+            function (response) {
+              console.log(response);
+              $scope.verificarLogin()    
+            },
+            function (response) {
+              console.log(response);
+              alert('Erro no Login!');
+            });
+      };
 });
