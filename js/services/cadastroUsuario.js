ad1fb8d33113d607c5255533e8adec827bb787cf
Luis Felipe <lipevasconcelos231@gmail.com>
2019-12-18 09:29:23 -0300

    Rota removida

--- js/services/cadastroUsuario.js
+++ js/services/cadastroUsuario.js
@@ -4,6 +4,16 @@ angular.module('ikbaseApp').factory('cadastroUsuario', function($http){
     });
 
     function cadastrarUsuario(usuario){
-        return $http.post('caminho', usuario);
+        return $http.post('localhost:8080/create/user/' + usuario);
     }
+
+    function pegaUsuario(usuario){
+        return $http.get('localhost:8080/get/user/:id' + usuario);
+    }
+
+    function deletaUsuario(usuario){
+        return $http.get('localhost:8080/get/user/:id' + usuario);
+    }
+
+
 });
